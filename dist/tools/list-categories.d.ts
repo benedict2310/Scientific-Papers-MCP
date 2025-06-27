f6dac97e16e63e07b8fdf37aaae8e43aa7686abf
Benedict Bleimschein <benedict.bleimschein@gmail.com>
2025-06-27 09:37:33 +0200

    chore: untrack dist directory

--- dist/tools/list-categories.d.ts
+++
@@ -1,3 +0,0 @@
-import { CategoryList } from '../types/papers.js';
-import { ListCategoriesParams } from '../config/schemas.js';
-export declare function listCategories(params: ListCategoriesParams): Promise<CategoryList>;
--- dist/types/sources.js
+++
@@ -1,2 +0,0 @@
-export {};
-//# sourceMappingURL=sources.js.map
